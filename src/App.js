83a036253e3fc55dcc8da0cbb12d05939fdd4abc
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2022-12-05 10:24:24 -0300

    start no react

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edite <code>src/App.js</code> e salve para recarregar.
         </p>
         <a
           className="App-link"
