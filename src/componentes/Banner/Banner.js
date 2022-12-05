dceffe63c469ce2aaa3c19fd9d60d97f87617fd0
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2022-12-05 11:12:22 -0300

    Criação da capa

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,14 @@
+import './Banner.css'
+
+function Banner () {
+    // JSX
+    return (
+            //no react usamos calssName e não class
+        <header className='banner'> 
+            <img src="/imagens/banner.png" alt="O banner principal da página" />
+        </header>
+        
+    )
+}
+
+export default Banner
