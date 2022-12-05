05a89c8c2ca6285ea611b9c5e5c33dacb7742383
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2022-12-05 12:55:22 -0300

    finalização do form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 // o import não precisa da extenção no final
 import Banner from './componentes/Banner';
-import CampoTexto from './componentes/CampoTexto';
+import Formulario from './componentes/Formulario';
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      <CampoTexto label="Nome" placeholder = "Digite seu nome"/>
-      <CampoTexto label="Cargo" placeholder = "Digite seu cargo"/>
-      <CampoTexto label="Imagem" placeholder = "Digite o endereço da imagem"/>
+      <Formulario />
     </div>
   );
 }
