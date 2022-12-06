3471fbd5eba2f214a024fd7c4625dcd0cd10f8ba
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2022-12-06 14:40:23 -0300

    Criando componente time

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
+import Time from './componentes/Time';
 
 function App() {
 
@@ -9,7 +10,7 @@ function App() {
   const [colaboradores, setColaboradores] = useState([]);
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
-    
+    setColaboradores([])
 
   };
 
@@ -17,6 +18,13 @@ function App() {
     <div className="App">
       <Banner />
       <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)} />
+      <Time nome="Gerente"/>
+      <Time nome="Programador"/>
+      <Time nome="Lider de equipe"/>
+      <Time nome="Aux administrativo"/>
+      <Time nome="Aux de Serviços gerais"/>
+      <Time nome="Assistente de rastreamento"/>
+      <Time nome="Motorista"/>
     </div>
   );
 }
