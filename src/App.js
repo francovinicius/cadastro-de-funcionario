065562341c7bd2a693e7ff97026243f7a92740b3
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2022-12-07 13:15:29 -0300

    setando cores para os times

--- src/App.js
+++ src/App.js
@@ -6,6 +6,44 @@ import Time from './componentes/Time';
 
 function App() {
 
+  const times = [
+    {
+      nome: 'Gerente',
+      corPrimaria: '#57c278',
+      corSecundaria: '#d9f7e9',
+    }
+    {
+      nome: 'Front-End',
+      corPrimaria: '#82CFFA',
+      corSecundaria: '#E8F8FF',
+    }
+    {
+      nome: 'Data Sciense',
+      corPrimaria: '#A6D157',
+      corSecundaria: '#F0F8E2',
+    }
+    {
+      nome: 'Devops',
+      corPrimaria: '#E06B69
+      corSecundaria: '#FDE7E8
+    }
+    {
+      nome: 'UX e Design',
+      corPrimaria: '#D86EBF',
+      corSecundaria: '#FAE95F5',
+    }
+    {
+      nome: 'Mobile',
+      corPrimaria: '#FEBA05',
+      corSecundaria: '#FFF5D9',
+    }
+    {
+      nome: 'Inovação e Gestão',
+      corPrimaria: '#FF8A29',
+      corSecundaria: '#FFEEDF',
+    }
+  ]
+
       //para concatenar os colaboradores
   const [colaboradores, setColaboradores] = useState([]);
 
