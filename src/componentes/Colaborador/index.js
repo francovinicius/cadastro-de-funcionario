0f30f776c0bcf5cc1a4d3a23ce05b87dee7f7aaf
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2022-12-08 09:06:54 -0300

    Criando a seção dos cards

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (
+        <div>
+            <div>
+                <img src='https://github.com/francovinicius.png' alt='' />
+            </div>
+            <div>
+                <h4>Vinicius</h4>
+                <h5>Programador</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
